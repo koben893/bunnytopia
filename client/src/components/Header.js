9c3d80b3ffc94f29f3f2855bafab1daa177c32d0
Andrew OBrien <aobrienaf@gmail.com>
2023-08-29 22:52:30 -0500

    logs are ont page with the right info

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,13 +6,10 @@ function Header() {
     return (
         <div>
             <DarkMode />
-                
             <div>
-                <img className='banner-img' src="https://img.freepik.com/free-vector/restaurant-rate-customer-review-isometric-banner_107791-1071.jpg?w=900&t=st=1691551975~exp=1691552575~hmac=99e3fc931948c31128d9698b49ff2665747c946d291f31e94634e27d3112fb07" alt='' /> <br />
+                <img className='banner-img' src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ebe3ea08-6d15-4b56-a300-abd30049dcbe/dfxyilh-e0c58ef0-afd2-4af5-8066-a45109c939a0.jpg/v1/fill/w_1280,h_536,q_75,strp/256bec45_9788_49ea_ae4d_1db0d554ef1d_by_log14_dfxyilh-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTM2IiwicGF0aCI6IlwvZlwvZWJlM2VhMDgtNmQxNS00YjU2LWEzMDAtYWJkMzAwNDlkY2JlXC9kZnh5aWxoLWUwYzU4ZWYwLWFmZDItNGFmNS04MDY2LWE0NTEwOWM5MzlhMC5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.gKWfc5d-q61fmakh4ApGZdfy4BBmTl0jjf0gVOzQ33Y" alt='' /> <br />
             </div>
-
             <NavBar />
-
         </div>
     );
 }
--- client/src/components/Logs.js
+++ client/src/components/Logs.js
@@ -4,28 +4,31 @@ function Logs({ user, onLogin }) {
     const [logs, setLogs] = useState([]);
 
     useEffect(() => {
-        fetch("http://127.0.0.1:5557/logs")
+        fetch("http://127.0.0.1:5557/logs")  // Update the URL to match your server's address
             .then((response) => response.json())
             .then((data) => {
-                console.log(data); // Check the response data in the console
+                console.log(data);
                 setLogs(data);
             })
             .catch((error) => {
                 console.error("Error fetching logs:", error);
             });
     }, []);
-    console.log(logs);
+
     return (
         <div className="header">
-            <h1>Top Logs</h1>
+            <h1 className="text-center">Rabbit Ages</h1>
             <div className="grid-container">
                 {logs.map((log) => (
                     <div key={log.id} className="card">
-                        <h3>Log: {log.log}</h3>
-                        <p>User: {log.user_name}</p>
-                        <p>Bunny: {log.bunny_name}</p>
+                        <h3>Days Old: {log.log}</h3>
+                        <p>Owner: {log.user_name}</p>
+                        <p>Bunny Name: {log.bunny_name}</p>
                         <button className="cardbutton" type="submit">
-                            Favorites
+                            Slaughter
+                        </button>
+                        <button className="cardbutton" type="submit">
+                            Breed
                         </button>
                     </div>
                 ))}
