6ef650ed0e29d514c62fab3e74d1b6a9efde3ba3
Andrew OBrien <aobrienaf@gmail.com>
2023-09-20 19:04:25 -0500

    fetch request in the schedule component

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Bunnies.css';
 
-function Schedule({ selectedBunnies }) {
+function Schedule({selectedBunnies}) {
+    const [scheduledBunnies, setScheduledBunnies] = useState([]); // Change the state variable name
+
+    useEffect(() => {
+        fetch("/breeding")
+            .then((response) => response.json())
+            .then((data) => setScheduledBunnies(data))
+        .catch((error) => {
+            console.error("Error getting bunny:", error);
+        });
+    }, []);
+
     return (
         <div className="header">
             <h1 className="center">Selected Bunnies for Breeding:</h1>
             <div className="grid-container">
-            {selectedBunnies?.map((bunnyId) => (
-                <div key={bunnyId.id} className="card">
-                <h1>
-                {bunnyId.logs}
-                </h1>
-                <h3>{bunnyId.name}</h3>
-            </div>
-            ))}
+                {selectedBunnies?.map((bunny) => (
+                    <div key={bunny.id} className="card">
+                    <h1>{bunny.logs}</h1>
+                    <h3>{bunny.name}</h3>
+                    </div>
+                ))}
             </div>
         </div>
     );
